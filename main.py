0832c4fac8e291ed22d821cdcb6638ea34c99552
Jnayna 🐾 Jinene Ben Said <101910279+Jinene@users.noreply.github.com>
2026-01-22 21:19:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from recommend import recommend, load_data
 # Load movie data
 movies, ratings = load_data()
 
+
+
 print("🎬 Welcome to the Movie Recommendation System! 🎬")
 movie_name = input("Enter a movie name: ")
 
@@ -11,5 +13,8 @@ try:
     print("\nTop 10 movie recommendations:")
     for i, movie in enumerate(recommendations, 1):
         print(f"{i}. {movie}")
+
+
+# error handling
 except:
     print("❌ Movie not found. Please check spelling or try another movie.")
